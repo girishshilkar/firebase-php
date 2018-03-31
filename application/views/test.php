27d3731b9a88ffee9d832b2e85aedc8455c21dc4
unknown <girishshilkar@gmail.com>
2018-03-31 12:16:40 +0530

    first commit

---
+++ application/views/test.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH.'third_party/vendor/autoload.php';
+
+const DEFAULT_URL = 'https://kidsplace.firebaseio.com/';
+const DEFAULT_TOKEN = 'MqL0c8tKCtheLSYcygYNtGhU8Z2hULOFs9OKPdEp';
+const DEFAULT_PATH = '/firebase/example';
+
+$firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
+
+// --- storing an array ---
+$test = array(
+    "foo" => "bar",
+    "i_love" => "lamp",
+    "id" => 42
+);
+$dateTime = new DateTime();
+$firebase->set(DEFAULT_PATH . '/' . $dateTime->format('c'), $test);
+
+// --- storing a string ---
+$firebase->set(DEFAULT_PATH . '/name/contact001', "John Doe");
+
+// --- reading the stored string ---
+$name = $firebase->get(DEFAULT_PATH . '/name/contact001');
